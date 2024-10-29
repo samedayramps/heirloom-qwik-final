6096a207da2e627866846eff1956c9701b661492
samedayramps <ty@samedayramps.com>
2024-10-29 18:31:02 -0500

    fix: commit changes

--- src/components/fonts/font-loader.tsx
+++ src/components/fonts/font-loader.tsx
@@ -1,6 +1,8 @@
 import { component$ } from '@builder.io/qwik';
 
 export const FontLoader = component$(() => {
+  const EPHESIS_URL = 'https://fonts.googleapis.com/css2?family=Ephesis&display=swap';
+  
   return (
     <>
       {/* Preload self-hosted fonts */}
@@ -19,7 +21,7 @@ export const FontLoader = component$(() => {
         crossOrigin="anonymous"
       />
 
-      {/* Google Fonts for Ephesis only */}
+      {/* Optimized Google Fonts loading for Ephesis */}
       <link 
         rel="preconnect" 
         href="https://fonts.googleapis.com" 
@@ -30,8 +32,17 @@ export const FontLoader = component$(() => {
         href="https://fonts.gstatic.com" 
         crossOrigin="anonymous"
       />
+      
+      {/* Preload Google Fonts CSS */}
       <link 
-        href="https://fonts.googleapis.com/css2?family=Ephesis&display=swap" 
+        rel="preload"
+        href={EPHESIS_URL}
+        as="style"
+      />
+      
+      {/* Load Google Fonts */}
+      <link 
+        href={EPHESIS_URL}
         rel="stylesheet"
       />
 
@@ -56,35 +67,6 @@ export const FontLoader = component$(() => {
             src: url('/fonts/open-sans-latin.woff2') format('woff2');
             unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
           }
-
-          /* Font fallbacks */
-          .font-playfair {
-            font-family: "Playfair Display", Georgia, serif;
-            font-optical-sizing: auto;
-            font-weight: 400;
-            font-style: normal;
-          }
-          
-          .font-opensans {
-            font-family: "Open Sans", system-ui, -apple-system, sans-serif;
-            font-optical-sizing: auto;
-            font-style: normal;
-            font-variation-settings: "wdth" 100;
-          }
-          
-          .font-ephesis {
-            font-family: "Ephesis", cursive;
-            font-weight: 400;
-            font-style: normal;
-            font-size: 1.6em;
-            line-height: 1;
-          }
-
-          /* Font variations */
-          .font-opensans-light { font-weight: 300; }
-          .font-opensans-regular { font-weight: 400; }
-          .font-opensans-medium { font-weight: 500; }
-          .font-opensans-semibold { font-weight: 600; }
         `}
       </style>
     </>
