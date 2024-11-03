9b54152108cda6a5bb997824a734cb0f2fd80cfb
samedayramps <ty@samedayramps.com>
2024-11-03 13:35:45 -0600

    feat: add service worker and optimize image loading

--- src/components/fonts/font-loader.tsx
+++ src/components/fonts/font-loader.tsx
@@ -8,44 +8,35 @@ export const FontLoader = component$(() => {
       {/* Preload self-hosted fonts */}
       <link 
         rel="preload"
-        href="/fonts/playfair-display-latin.woff2" 
+        href="/fonts/PlayfairDisplay-Regular.woff2" 
         as="font"
         type="font/woff2"
         crossOrigin="anonymous"
       />
       <link
         rel="preload" 
-        href="/fonts/open-sans-latin.woff2"
+        href="/fonts/OpenSans-Regular.woff2"
         as="font"
         type="font/woff2"
         crossOrigin="anonymous"
       />
-
-      {/* Preload critical images */}
       <link
-        rel="preload"
-        href="/assets/images/16-texture-square.webp"
-        as="image"
-        type="image/webp"
+        rel="preload" 
+        href="/fonts/OpenSans-Italic.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous"
       />
 
       {/* Optimized Google Fonts loading for Ephesis */}
       <link 
         rel="preconnect" 
         href="https://fonts.googleapis.com" 
-        crossOrigin="anonymous"
       />
       <link 
         rel="preconnect" 
         href="https://fonts.gstatic.com" 
-        crossOrigin="anonymous"
-      />
-      
-      {/* Preload Google Fonts CSS */}
-      <link 
-        rel="preload"
-        href={EPHESIS_URL}
-        as="style"
+        crossOrigin=""
       />
       
       {/* Load Google Fonts */}
@@ -55,28 +46,37 @@ export const FontLoader = component$(() => {
       />
 
       {/* Font-face declarations */}
-      <style>
-        {`
+      <style
+        dangerouslySetInnerHTML={`
           /* Self-hosted fonts */
           @font-face {
             font-family: 'Playfair Display';
             font-style: normal;
-            font-weight: 400 700;
+            font-weight: 400;
             font-display: swap;
-            src: url('/fonts/playfair-display-latin.woff2') format('woff2');
+            src: url('/fonts/PlayfairDisplay-Regular.woff2') format('woff2');
             unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
           }
 
           @font-face {
             font-family: 'Open Sans';
             font-style: normal;
-            font-weight: 300 600;
+            font-weight: 400;
             font-display: swap;
-            src: url('/fonts/open-sans-latin.woff2') format('woff2');
+            src: url('/fonts/OpenSans-Regular.woff2') format('woff2');
+            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+          }
+
+          @font-face {
+            font-family: 'Open Sans';
+            font-style: italic;
+            font-weight: 400;
+            font-display: swap;
+            src: url('/fonts/OpenSans-Italic.woff2') format('woff2');
             unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
           }
         `}
-      </style>
+      />
     </>
   );
 }); 
