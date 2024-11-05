a5de9f551821499406ad32463c3b7b8aecd7a8c1
samedayramps <ty@samedayramps.com>
2024-11-05 15:52:56 -0600

    feat: add sitemap generation

--- src/routes/sitemap.xml/index.tsx
+++ src/routes/sitemap.xml/index.tsx
@@ -1,9 +1,14 @@
 import type { RequestHandler } from '@builder.io/qwik-city';
 import { createSitemap } from './create-sitemap';
 import { CONTENT } from '../films/content';
+import BLOG_POSTS from '../blog/[slug]/index';
 
 export const onGet: RequestHandler = ({ url, send }) => {
   const baseUrl = url.origin;
+
+  // Log CONTENT data
+  console.log('CONTENT in production:', CONTENT);
+
   const staticRoutes = [
     { loc: '/', priority: 1.0 },
     { loc: '/about', priority: 0.8 },
@@ -11,22 +16,22 @@ export const onGet: RequestHandler = ({ url, send }) => {
     { loc: '/blog', priority: 0.8 },
   ];
 
-  console.log('Static Routes:', staticRoutes);
-
   const filmRoutes = CONTENT.films.map(film => ({
     loc: `/films/${film.slug}`,
     priority: 0.8,
   }));
 
-  console.log('Film Routes:', filmRoutes);
+  const blogRoutes = Object.keys(BLOG_POSTS).map(slug => ({
+    loc: `/blog/${slug}`,
+    priority: 0.8,
+  }));
 
-  const allRoutes = [...staticRoutes, ...filmRoutes];
+  const allRoutes = [...staticRoutes, ...filmRoutes, ...blogRoutes];
 
+  // Log all routes
   console.log('All Routes:', allRoutes);
 
   const sitemap = createSitemap(allRoutes, baseUrl);
-
-  console.log('Generated Sitemap:', sitemap);
 
   send(new Response(sitemap, {
     headers: {
