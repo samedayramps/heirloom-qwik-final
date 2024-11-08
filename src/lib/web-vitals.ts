9e5d5ea1a6ef82cc8e3ab09789264160a38d723e
samedayramps <ty@samedayramps.com>
2024-11-08 13:44:28 -0600

    feat: update film content

--- src/lib/web-vitals.ts
+++ src/lib/web-vitals.ts
@@ -3,16 +3,18 @@ declare const gtag: Function;
 
 import { onCLS, onFID, onLCP } from 'web-vitals';
 
-function sendToAnalytics(metric: any) {
-  const { name, value } = metric;
-  gtag('event', name, {
-    event_category: 'Web Vitals',
-    event_label: name,
-    value: Math.round(name === 'CLS' ? value * 1000 : value), // Google Analytics expects integers
-    non_interaction: true, // Avoid affecting bounce rate
-  });
-}
+if (typeof window !== 'undefined') {
+  function sendToAnalytics(metric: any) {
+    const { name, value } = metric;
+    gtag('event', name, {
+      event_category: 'Web Vitals',
+      event_label: name,
+      value: Math.round(name === 'CLS' ? value * 1000 : value), // Google Analytics expects integers
+      non_interaction: true, // Avoid affecting bounce rate
+    });
+  }
 
-onCLS(sendToAnalytics);
-onFID(sendToAnalytics);
-onLCP(sendToAnalytics); 
+  onCLS(sendToAnalytics);
+  onFID(sendToAnalytics);
+  onLCP(sendToAnalytics);
+} 
